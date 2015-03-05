63c7909528528c443641f350814ff9a2a38df503
expez <expez@expez.com>
2015-03-05 19:15:15 +0100

    Get rid of leiningen plugins I rarely use
    fucking classpath is getting polluted

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,5 @@
 {:user {:plugins [[lein-localrepo "0.5.3"]
-                  [lein-try "0.4.1"]
-                  [lein-pprint "1.1.1"]
-                  [lein-ancient "0.6.2"]
-                  [lein-describe "0.2.0"]
-                  [lein-exec "0.3.2"]
-                  [lein-simpleton "1.2.0"]
+                  [lein-ancient "0.6.3"]
                   [refactor-nrepl "0.3.0-SNAPSHOT"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
                   [lein-vanity "0.2.0"]]
@@ -17,19 +12,14 @@
                        [print-foo "1.0.1"]
                        [spyscope "0.1.5"]
                        [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]
-                       [leiningen #=(leiningen.core.main/leiningen-version)]
-                       [io.aviso/pretty "0.1.16"]
                        [com.cemerick/pomegranate "0.3.0"]
                        [im.chit/vinyasa "0.3.0"]]
         :injections [(require 'spyscope.core)
                      (require '[vinyasa.inject :as inject])
-                     (require 'io.aviso.repl)
-
                      (inject/in ;; the default injected namespace is `.`
 
                       ;; note that `:refer, :all and :exclude can be used
                       [vinyasa.inject :refer [inject [in inject-in]]]
-                      [vinyasa.lein :exclude [*project*]]
 
                       ;; imports all functions in vinyasa.pull
                       [vinyasa.pull :all]
