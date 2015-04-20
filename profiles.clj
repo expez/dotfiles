e92d4b1ff11b0e46f8744d8255d24f80ce4828c0
expez <expez@expez.com>
2015-04-20 19:35:42 +0200

    bump deps

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,8 @@
 {:user {:plugins [[lein-localrepo "0.5.3"]
                   [lein-ancient "0.6.3"]
-                  [refactor-nrepl "0.3.0-SNAPSHOT"]
+                  [refactor-nrepl "1.0.4-SNAPSHOT"]
                   [cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                  [com.jakemccrary/lein-test-refresh "0.7.0"]
                   [lein-vanity "0.2.0"]]
         :repl-options {:init (do
                                (set! *print-length* 200)
@@ -14,13 +15,13 @@
         :dependencies [[pjstadig/humane-test-output "0.6.0"]
                        [print-foo "1.0.1"]
                        [spyscope "0.1.5"]
+                       [alembic "0.3.2"]
                        [acyclic/squiggly-clojure "0.1.2-SNAPSHOT"]
                        [im.chit/vinyasa.inject "0.3.4"]
                        [im.chit/vinyasa.reimport "0.3.4"]
                        [im.chit/vinyasa.reflection "0.3.4"]
                        [im.chit/vinyasa.lein "0.3.4"]
-                       [leiningen #=(leiningen.core.main/leiningen-version)]
-                       [alembic "0.3.2"]]
+                       [leiningen #=(leiningen.core.main/leiningen-version)]]
         :injections [(require '[vinyasa.inject :as inject])
                      (inject/in ;; the default injected namespace is `.`
 
