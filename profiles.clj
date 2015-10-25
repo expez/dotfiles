b90f8aa3151922b7d883476bf60cc3ab79711e4a
Lars Andersen <expez@expez.com>
2015-10-25 14:52:18 +0100

    Bump refactor-nrepl version

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[lein-localrepo "0.5.3"]
                   [lein-ancient "0.6.3"]
                   [lein-license "0.1.3"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]
                   [cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.9.0"]
                   [lein-vanity "0.2.0"]]
