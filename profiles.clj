ea51feffc3ea1e10d7de3bb7a11e5340d17e514c
Lars Andersen <expez@expez.com>
2016-01-14 10:44:03 +0100

    Bump cider snapshot

--- profiles.clj
+++ profiles.clj
@@ -9,7 +9,7 @@
         :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
         :dependencies [[pjstadig/humane-test-output "0.6.0"]]}
  :repl {:plugins [[refactor-nrepl "2.0.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]]
+                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
         :dependencies [[print-foo "1.0.1"]
                        [spyscope "0.1.5"]
                        [alembic "0.3.2"]
@@ -17,9 +17,7 @@
                        [im.chit/vinyasa.inject "0.3.4"]
                        [im.chit/vinyasa.reimport "0.3.4"]
                        [im.chit/vinyasa.reflection "0.3.4"]
-                       [org.clojure/tools.nrepl "0.2.12"]
-                       ;; [leiningen #=(leiningen.core.main/leiningen-version)]
-                       ]
+                       [org.clojure/tools.nrepl "0.2.12"]]
         :repl-options {:init (do
                                (set! *print-length* 200)
                                (require '[spyscope.core]
