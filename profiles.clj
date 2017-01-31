2f468e3ad40a3806682a568adc916ca9920b660d
Lars Andersen <expez@expez.com>
2017-01-31 11:47:21 +0100

    Bump deps in profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,18 +1,18 @@
-{:user {:plugins [[lein-localrepo "0.5.3" :exclusions [org.clojure/clojure]]
-                  [lein-ancient "0.6.3" :exclusions [org.clojure/clojure]]
-                  [lein-license "0.1.3" :exclusions [org.clojure/clojure]]
-                  [com.jakemccrary/lein-test-refresh "0.9.0" :exclusions [org.clojure/clojure]]
-                  [lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]
-                  [lein-environ "1.0.2"]]
+{:user {:plugins [
+                  [lein-localrepo "0.5.3" :exclusions [org.clojure/clojure]]
+                  [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
+                  [lein-license "0.1.6" :exclusions [org.clojure/clojure]]
+                  [com.jakemccrary/lein-test-refresh "0.18.1" :exclusions [org.clojure/clojure]]
+                  [lein-vanity "0.2.0" :exclusions [org.clojure/clojure]]]
         :license {:author "Lars Andersen"
                   :email  "expez@expez.com"}
         :test-refresh {:quiet true}
         :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
-        :dependencies [[pjstadig/humane-test-output "0.6.0"]]}
- :repl {:dependencies [[print-foo "1.0.1"]
+        :dependencies [[pjstadig/humane-test-output "0.8.1"]]}
+ :repl {:dependencies [[print-foo "1.0.2"]
                        [spyscope "0.1.6"]
                        [alembic "0.3.2"]
-                       [acyclic/squiggly-clojure "0.1.5"]
+                       [acyclic/squiggly-clojure "0.1.6"]
                        [im.chit/vinyasa.inject "0.4.7"]
                        [im.chit/vinyasa.reimport "0.4.7"]
                        [im.chit/vinyasa.reflection "0.4.7"]]
@@ -31,7 +31,7 @@
 
                       ;; inject into clojure.core with prefix
                       clojure.core >
-                      ;; [vinyasa.reimport reimport]
+                      [vinyasa.reimport reimport]
                       [clojure.pprint pprint]
                       [clojure.java.shell sh]
                       [clojure.repl :all])]}}
